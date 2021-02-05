ed342db56434854cd174d176913403884d69fc23
Chris Joakim <christopher.joakim@gmail.com>
2021-02-05 18:58:33 -0500

    eod 2/5

--- dev.py
+++ dev.py
@@ -2,6 +2,7 @@ import json
 import sys
 import time
 import os
+import traceback
 
 import arrow 
 
@@ -9,14 +10,32 @@ import gdg
 
 if __name__ == "__main__":
     print('main')
-    g = gdg.Gdg('data/date_based/.gdg')
-    print(g)
+    g = gdg.Gdg('data/gen_based', True)
+
     try:
         print('set_generations...')
         print(g.set_generations(10))
         print('set_pattern...')
-        print(g.set_pattern('test---{}---{}---{}---{}.txt', ['e', 'ts_utc', 'ts_local', 'g']))
+        print(g.set_pattern('sample-%.txt', 'g'))
+  
         print('next...')
         print(g.next())
-    except:
-        print(g)
+
+        files_list = g.all()
+        for f in files_list:
+            print('all: {}'.format(f))
+
+        print('current...')
+        print(g.current())
+
+        print('previous...')
+        print(g.previous())
+
+        print('get_state...')
+        print(g.get_state())
+
+        print('next...')
+        print(g.next())
+
+    except Exception as e:
+        traceback.print_exc()
--- gdg/gdg.py
+++ gdg/gdg.py
@@ -8,6 +8,7 @@ import time
 import traceback
 
 from datetime import datetime, date, timezone
+from inspect import currentframe, getframeinfo
 
 FORMAT_GENERATION = 'g'
 FORMAT_EPOCH      = 'e'
@@ -20,23 +21,39 @@ VALID_FORMATS = [
     FORMAT_TIMESTAMP_LOCAL
 ]
 
-FORMAT_TIMESTAMP = '%Y-%m-%d-%H:%M:%S'
+TIMESTAMP_FORMAT = '%Y-%m-%d-%H:%M:%S'
 
-RE_GENERATION = '\d\d\d\d\d\d'  # 6 digits
-RE_EPOCH      = '\d\d\d\d\d\d\d\d\d\d'  # 1612529215 (10 digits)
-RE_TIMESTAMP  = '\d\d\d\d-\d\d-\d\d-\d\d:\d\d:\d\d'  # 2021-02-05-07:56:23
+PARAMETER_CHAR = '%'
+
+RE_TOKEN_MAP = dict()
+RE_TOKEN_MAP[FORMAT_GENERATION] = '\d\d\d\d\d\d'  # 6 digits
+RE_TOKEN_MAP[FORMAT_EPOCH]      = '\d\d\d\d\d\d\d\d\d\d'  # 1612529215 (10 digits)
+RE_TOKEN_MAP[FORMAT_TIMESTAMP_UTC]   = '\d\d\d\d-\d\d-\d\d-\d\d:\d\d:\d\d'  # 2021-02-05-07:56:23
+RE_TOKEN_MAP[FORMAT_TIMESTAMP_LOCAL] = '\d\d\d\d-\d\d-\d\d-\d\d:\d\d:\d\d'  # 2021-02-05-07:56:23
+
+RE_GENERATIION_NUMBER = '*\d\d\d\d\d\d*'
 
 
 class Gdg(object):
     """
 
     """
-    def __init__(self, path='.gdg', verbose=False):
-        self.path = path
-        self.state = self.__read_state()
+    def __init__(self, directory, verbose=False):
+        self.directory = directory.strip()
+        if self.directory.endswith(os.path.sep):
+            self.directory = self.directory[:-1]
         self.verbose = verbose
+        self.metafile = '{}{}.gdg'.format(self.directory, os.path.sep)
+        self.state = self.__read_state()
+        self.__write_state()
+
         if self.verbose:
-            print('init; path: {} state: {}'.format(self.path, self.state))
+            print('init, directory: {}'.format(self.directory))
+            print('init, metafile:  {}'.format(self.metafile))
+            print(str(self))
+
+    def get_state(self):
+        return self.__read_state()
 
     def set_generations(self, n):
         if int(n) > 0:
@@ -48,13 +65,15 @@ class Gdg(object):
             self.state['max_days'] = int(n)
             self.__write_state()
 
-    def set_pattern(self, pattern, param_list):
+    def set_pattern(self, pattern, value_param):
         try:
             curr_state = self.__read_state()  # capture current state in case of restore
             self.state['pattern'] = pattern.strip()
-            self.state['param_list'] = self.__parse_parm_list(param_list)
-            self.state['regexp'] = self.__set_regexp()
+            self.state['value_param'] = self.__parse_value_param(value_param)
+            self.state['regexp'] = self.__format_regexp()
+            print('set_pattern before next()')
             s = self.next()  # will throw an exception if pattern is invalid, discard s
+            print('set_pattern after next()')
             return True
         except Exception as e:
             self.state = curr_state  # restore the state from before this method invocation
@@ -71,26 +90,55 @@ class Gdg(object):
         #return 'first: {} second:{}'.format(*['a', 'b'])
         template = self.state['pattern']
         values = list()
-        for p in self.state['param_list']:
-            if p == FORMAT_GENERATION:
-                n = 1 # todo, read fs to get current n
-                values.append('{0:06d}'.format(n))
-            if p == FORMAT_EPOCH:
-                values.append(int(time.time()))
-            if p == FORMAT_TIMESTAMP_UTC:
-                values.append(datetime.utcnow().strftime(FORMAT_TIMESTAMP))
-            if p == FORMAT_TIMESTAMP_LOCAL:
-                values.append(datetime.now().strftime(FORMAT_TIMESTAMP))
+        p = self.state['value_param']
+        if p == FORMAT_GENERATION:
+            f = self.current()
+            n = 1
+
+            print('next, current: {}'.format(f))
+            if f == None:
+                n = 1
+            else:
+                compiled_re = re.compile('{}{}{}'.format(
+                    self.directory, os.path.sep, RE_GENERATIION_NUMBER))
+                m = compiled_re.match(f)
+                print(m)
+
+            #     n = 2 # todo, read fs to get current n
+            values.append('{0:06d}'.format(n))
+        if p == FORMAT_EPOCH:
+            values.append(int(time.time()))
+        if p == FORMAT_TIMESTAMP_UTC:
+            values.append(datetime.utcnow().strftime(TIMESTAMP_FORMAT))
+        if p == FORMAT_TIMESTAMP_LOCAL:
+            values.append(datetime.now().strftime(TIMESTAMP_FORMAT))
         return template.format(*values)
 
     def current(self):
-        pass
+        all = self.all()
+        if len(all) > 0:
+            return all[-1]
+        else:
+            return None
 
     def previous(self):
-        pass
+        all = self.all()
+        if len(all) > 1:
+            return all[-2]
+        else:
+            return None
 
     def all(self):
-        pass
+        files_list, matched_list = self.__walk_fs(), list()
+        compiled_re = re.compile('{}{}{}'.format(
+            self.directory, os.path.sep, self.state['regexp']))
+        for f in files_list:
+            #print(f)
+            m = compiled_re.match(f)
+            if m != None:
+                print(m)
+                matched_list.append(f)
+        return sorted(matched_list)
 
     def prune(do_deletes=False):
         # return a list of filenames, optionally delete them
@@ -99,77 +147,42 @@ class Gdg(object):
     # dunder methods
 
     def __str__(self):
-        return "<Gdg path:{} state:{}>".format(self.path, json.dumps(self.state))
+        return "<Gdg directory:{} state:{}>".format(
+            self.directory, json.dumps(self.state))
         
     # private methods follow 
 
-    def __parse_parm_list(self, param_list):
-        parsed_param_list = list()
-        try:
-            for p in param_list:
-                if p.lower() in VALID_FORMATS:
-                    parsed_param_list.append(p.lower())
-                else:
-                    raise Exception('Invalid filename pattern parameters list: {}'.format(param_list))
-        except:
-            raise Exception('Invalid filename pattern parameters list: {}'.format(param_list))
-        return parsed_param_list
+    def __parse_value_param(self, value_param):
+        if value_param.lower() in VALID_FORMATS:
+            return value_param.lower()
+        else:
+            return None
 
-    def __set_regexp(self):
-        # self.state['pattern'] = pattern.strip()
-        # self.state['param_list']
-
-        # TODO - handle case where pattern starts and/or ends with {}?
-        pattern_tokens = self.state['pattern'].split('{}')   
-        regexp_tokens = list()
-        for token in pattern_tokens:
-            pass
-
-            # RE_GENERATION = '\d\d\d\d\d\d'  # 6 digits
-            # RE_EPOCH      = '\d\d\d\d\d\d\d\d\d\d'  # 1612529215 (10 digits)
-            # RE_TIMESTAMP  = '\d\d\d\d-\d\d-\d\d-\d\d:\d\d:\d\d'  # 2021-02-05-07:56:23
-
-        return 'todo'
+    def __format_regexp(self):
+        format_template = self.state['pattern'].replace('%', '{}')
+        format_values = list()
+        re_value = RE_TOKEN_MAP[self.state['value_param']] 
+        format_values.append(re_value)
+        return format_template.format(*format_values)
 
     def __walk_fs(self):
         files = list()
-        files_dir = ''  # TODO - get from self.path
-        for dir_name, subdirs, base_names in os.walk(files_dir):
+        for dir_name, subdirs, base_names in os.walk(self.directory):
             for base_name in base_names:
-                full_name = "{}/{}".format(dir_name, base_name)
-                entry = dict()
-                entry['base'] = base_name
-                entry['dir'] = dir_name
-                entry['full'] = full_name
-                entry['abspath'] = os.path.abspath(full_name)
-                files.append(entry)
+                relative_path = "{}/{}".format(dir_name, base_name)
+                files.append(relative_path)
         return files
 
     def __read_state(self):
         try:
-            with open(self.path, 'rt') as f:
+            with open(self.metafile, 'rt') as f:
                 return json.loads(f.read())
         except:
             return {}
 
     def __write_state(self):
-        with open(self.path, 'w') as f:
+        with open(self.metafile, 'w') as f:
             jstr = json.dumps(self.state)
             f.write(jstr)
             if self.verbose:
                 print('state:{}'.format(jstr))
-
-
-# g = Gdg(dir, name='.gdg')   # the spec file defaults to .gdg, but others can be created
-# g.set_generations(n)           # updates the .gdg file
-# g.set_max_days(n)           # updates the .gdg file
-# g.set_pattern(s, params)    # sets the filename pattern and parameters
-
-# fn = g.current()
-# fn = g.previous()
-# fn_list = g.all()
-# fn = g.next()
-# fn_list = g.prune_list(do_deletes=False, retain_count=-1)
-
-
-
