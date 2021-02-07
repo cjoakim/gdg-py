41ca804aac1e273988dfd72aac8a917cbebcf5ae
Chris Joakim <christopher.joakim@gmail.com>
2021-02-07 14:16:45 -0500

    tests/test_generation_type.py - wip

--- tests/test_generation_type.py
+++ tests/test_generation_type.py
@@ -32,15 +32,34 @@ def test_generation_number_type_gdg():
     bool = g.set_pattern('sample-%.txt', 'g')
     assert(bool == True)
 
-    state = g.get_state()
 
+    # assertions after .gdg state is established
+    state = g.get_state()
     assert(len(state.keys()) == 4)
     assert(state['generations'] == 10)
     assert(state['pattern']     == 'sample-%.txt')
     assert(state['value_param'] == 'g')
     assert(state['regexp']      == 'sample-\\d\\d\\d\\d\\d\\d.txt')
 
+    # assert initial set of files
+    assert(g.previous() == None)
+    assert(g.current() == None)
+    assert(g.all_generations() == [])
+    assert(g.all_files() == ['tmp/epoch/.gdg'])
 
+    assert(g.next() == 'tmp/epoch/sample-000001.txt')
+
+    for i in range(1, 21):
+        fname = g.next()
+        __write(fname, 'this if file {}'.format(i))
+    
+    g.prune()
+
+    assert(g.previous() == 'tmp/epoch/sample-000019.txt')
+    assert(g.current()  == 'tmp/epoch/sample-000020.txt')
+
+    # assert(g.all_generations() == ['tmp/epoch/.gdg'])
+    # assert(g.all_files() == ['tmp/epoch/.gdg'])
 
 # private methods
 
